8c29f60e1135105d5a8e16e5c57790b5552e2ff4
zhanghaojie <1455123087@qq.com>
2024-04-10 11:14:05 +0800

    Refactor SharedElements components and update usage in index.tsx

--- src/pages/docs/index.tsx
+++ src/pages/docs/index.tsx
@@ -1,38 +1,25 @@
 import { GrayBlock } from '@/components/GrayBlock';
-import {
-  SharedPosition,
-  SharedRect,
-  SharedSize,
-} from '@/components/SharedElements';
+import { SharedRect } from '@/components/SharedElements';
+import { useLocation } from 'umi';
+
+function useURLSharedItemId() {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  return searchParams.get('sharedItemId')!;
+}
 
 const DocsPage = () => {
+  const sharedItemId = useURLSharedItemId();
   return (
     <div>
-      <SharedSize<HTMLDivElement> sharedId="SharedSize">
-        {({ ref }) => <GrayBlock ref={ref} className="size-32" />}
-      </SharedSize>
+      <SharedRect<HTMLDivElement> sharedId={sharedItemId}>
+        {({ ref }) => <GrayBlock ref={ref} className="w-full h-80" />}
+      </SharedRect>
       <p>
         common began laugh space pony scientific location dinner value job hurt
         shaking broken send heavy slow quick stiff eager locate press largest
         she anyway
       </p>
-      <div className="flex flex-row space-x-2">
-        <GrayBlock className="size-24" />
-        <SharedRect<HTMLDivElement> sharedId="SharedRect">
-          {({ ref }) => <GrayBlock ref={ref} className="size-28" />}
-        </SharedRect>
-      </div>
-      <SharedPosition<HTMLDivElement> sharedId="SharedElement">
-        {({ ref }) => <GrayBlock ref={ref} />}
-      </SharedPosition>
-      <p>
-        balance cage composition more tax tank guide easily help while color
-        cutting pine brother torn draw though voyage feed musical yet finish
-        around put
-      </p>
-      <SharedPosition<HTMLDivElement> sharedId="SharedElement2">
-        {({ ref }) => <GrayBlock className="ms-8" ref={ref} />}
-      </SharedPosition>
     </div>
   );
 };
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,27 +1,40 @@
+import { Link } from 'umi';
 import { GrayBlock } from '@/components/GrayBlock';
-import {
-  SharedPosition,
-  SharedRect,
-  SharedSize,
-} from '@/components/SharedElements';
+import { SharedRect } from '@/components/SharedElements';
+
+const randomTexts = [
+  'supper place radio mainly process cloud else seen number flight shut fun mathematics window appearance clothes eaten blue down party cool spell black while',
+  'even valley apart both hung section activity surrounded process your attached citizen different review load wing center including gate universe wrong is interest straw',
+  'quick except flight beyond bush provide badly bare principal line evening never giant little spent winter am stay alphabet furniture seen wing build past',
+  'breathing baby direction railroad lucky mice single little grabbed trick week aware climate operation example probably whose shaking score steep boy someone important shake',
+  'rush species mixture phrase pay truth sweet become movie name correctly colony position brick game cave nest smell bicycle onto tent wood corn opposite',
+];
+
+const items = Array.from({ length: 8 }, (_, i) => ({
+  sharedItemId: `SharedItem${i + 1}`,
+  text: randomTexts[i % randomTexts.length],
+}));
 
 export default function HomePage() {
   return (
     <div>
-      <SharedSize<HTMLDivElement> sharedId="SharedSize">
-        {({ ref }) => <GrayBlock ref={ref} />}
-      </SharedSize>
-      <div className="mt-80" />
-      <SharedRect<HTMLDivElement> sharedId="SharedRect">
-        {({ ref }) => <GrayBlock ref={ref} />}
-      </SharedRect>
-      <SharedPosition<HTMLDivElement> sharedId="SharedElement">
-        {({ ref }) => <GrayBlock className="ms-16" ref={ref} />}
-      </SharedPosition>
-      <div className="mt-10" />
-      <SharedPosition<HTMLDivElement> sharedId="SharedElement2">
-        {({ ref }) => <GrayBlock className="ms-16" ref={ref} />}
-      </SharedPosition>
+      <ul className="flex flex-col space-y-2">
+        {items.map((item) => (
+          <li key={item.sharedItemId}>
+            <Link
+              to={`/docs?sharedItemId=${item.sharedItemId}`}
+              className="flex flex-row space-x-2"
+            >
+              <div className="size-36 overflow-visible">
+                <SharedRect<HTMLDivElement> sharedId={item.sharedItemId}>
+                  {({ ref }) => <GrayBlock ref={ref} className="size-36" />}
+                </SharedRect>
+              </div>
+              <div className="text-2xl">{item.text}</div>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
